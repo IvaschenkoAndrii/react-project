5a7abe9428f2ad3e9fa5c9faa862ebfc4f562b53
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-10-16 18:25:09 +0300

    v.1-slices,movies

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -3,9 +3,9 @@ import {NavLink} from "react-router-dom";
 function Movie({movie}) {
 
 
+
     return (
         <div>
-            {/*<img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}></img>*/}
             <NavLink to={`/movies/${movie.id}`}>
                 <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}></img>
                 <h4>{movie.title}</h4>
--- src/components/MovieDetailed/MovieDetailed.js
+++ src/components/MovieDetailed/MovieDetailed.js
@@ -1,20 +1,22 @@
-import {Await, useParams} from "react-router-dom";
+import {Await, useNavigate, useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 
 import {movieActions} from "../../redux";
 
-function MovieDetailed (){
+function MovieDetailed() {
 
     let {id} = useParams();
 
     const dispatch = useDispatch();
 
-    const {movie}=useSelector(state => state.movieReducer)
+    const {movie} = useSelector(state => state.movieReducer)
 
-    useEffect(()=>{
+    const navigate = useNavigate();
+
+    useEffect(() => {
         dispatch(movieActions.getMovie({id}));
-    },[id])
+    }, [id])
 
     console.log(movie);
 
@@ -24,8 +26,13 @@ function MovieDetailed (){
             <img src={`https://image.tmdb.org/t/p/w300${movie?.poster_path}`}></img>
             <p>{movie?.overview}</p>
             <p>Date of release {movie?.release_date}</p>
+            <p>Gen</p>
+
+            <div>
+                <button onClick={()=>navigate('/movies')}>Main Page</button>
+            </div>
+
         </div>
-    );
-}
+    );}
 
 export {MovieDetailed}

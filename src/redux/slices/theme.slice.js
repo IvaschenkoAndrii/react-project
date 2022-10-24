d6ad475c299e36c7769f509423cc8a973240232c
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-10-24 12:35:33 +0300

    almost finish

--- src/redux/slices/theme.slice.js
+++ src/redux/slices/theme.slice.js
@@ -2,17 +2,6 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
     themes: {}
-    // themes: {
-    //     header: 'header_light',
-    //     movies: 'movies_light',
-    //     searchForm: 'searchForm_light',
-    //     buttons: 'buttons_light',
-    //     card: 'card_light',
-    //     cardDetail:'cardDetail_light',
-    //     genries:'genries_light',
-    //     genriesCard:'genriesCards_light',
-    //     allCardsGenrie:'allCardsGenrie_light'
-    // }
 };
 
 const themeSlice = createSlice({

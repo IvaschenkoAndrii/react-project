e1c820b70ac0a46546c1de15d20f140489cb4433
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-10-24 19:44:52 +0300

    finish

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import {baseURL, token} from "../config";
 
-let axiosService = axios.create({baseURL});
+const axiosService = axios.create({baseURL});
 
 axiosService.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;

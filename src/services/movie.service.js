7666d7debb265fd4a04812f37065567e72f588b9
IvaschenkoAndrii <ivaschenko84@gmail.com>
2022-10-30 14:01:49 +0200

    new version

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -2,7 +2,7 @@ import {axiosService} from "./axios.service";
 import {urls} from "../config";
 
 const movieService = {
-    getAllByPages: (page=1) => axiosService.get(urls.movies,{params:{page}}),
+    // getAllByPages: (page=1) => axiosService.get(urls.movies,{params:{page}}),
     getMovie:(id)=>axiosService.get(urls.movie+'/'+id),
     getAllByGenrie: (with_genres,page=1) => axiosService.get(urls.movies,{params:{with_genres,page}})
 }
